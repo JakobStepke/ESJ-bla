1c24a9ec57f06debf3c7e66e3a3996412cfb76ba
Emil Pobinger <emil@pobinger.at>
2023-10-26 17:37:10 +0200

    f: fix-matrix

--- src/matrix.h
+++ src/matrix.h
@@ -7,7 +7,8 @@
 #include "vector.h"
 #include "ordering.h"
 
-namespace ASC_bla {
+namespace ASC_bla
+{
 
     template <typename T, ORDERING ORD>
     class MatrixView : public MatExpr<MatrixView<T, ORD>, ORD>
@@ -15,134 +16,171 @@ namespace ASC_bla {
     protected:
         size_t rows_;
         size_t cols_;
-        Vector<T> * data_;
+        Vector<T> *data_;
+
     public:
-        MatrixView (size_t rows, size_t cols, T * data)
-        : rows_(rows), cols_(cols), data_(data) { }
-        
+        MatrixView(size_t rows, size_t cols, Vector<T> *data)
+            : rows_(rows), cols_(cols), data_(data) {}
+
         template <typename TB, ORDERING ORDB>
-        MatrixView & operator= (const MatExpr<TB, ORDB> & m2)
+        MatrixView &operator=(const MatExpr<TB, ORDB> &m2)
         {
             rows_ = m2.Size_Cols();
             cols_ = m2.Size_Cols();
             data_ = m2.Data();
 
-        return *this;
+            return *this;
         }
 
-        MatrixView & operator= (T scal)
+        MatrixView &operator=(T scal)
         {
             rows_ = scal;
             cols_ = scal;
             data_ = 0;
-        return *this;
+            return *this;
         }
-        
+
         auto View() const { return MatrixView(rows_, cols_, data_); }
         size_t Size_Cols() const { return cols_; }
         size_t Size_Rows() const { return rows_; }
 
-        
-
-        T & operator()(size_t i, size_t j) { 
-            if constexpr (ORD == ORDERING::ColMajor) {
+        T &operator()(size_t i, size_t j)
+        {
+            if constexpr (ORD == ORDERING::ColMajor)
+            {
                 return data_[i](j * rows_);
-            } else {
+            }
+            else
+            {
                 return data_[j](i * cols_);
             }
         }
-        const T & operator()(size_t i, size_t j) const { 
-            if constexpr (ORD == ORDERING::ColMajor) {
+        const T &operator()(size_t i, size_t j) const
+        {
+            if constexpr (ORD == ORDERING::ColMajor)
+            {
                 return data_[i](j * rows_);
-            } else {
+            }
+            else
+            {
                 return data_[j](i * cols_);
             }
-         }
+        }
 
-        auto Transpose() const {
-            if constexpr (ORD == ORDERING::ColMajor) {
+        auto Transpose() const
+        {
+            if constexpr (ORD == ORDERING::ColMajor)
+            {
                 return MatrixView<T, ORDERING::RowMajor>(cols_, rows_, data_);
-            } else {
+            }
+            else
+            {
                 return MatrixView<T, ORDERING::ColMajor>(cols_, rows_, data_);
             }
         }
 
-         auto Row(size_t i) {
-                if constexpr (ORD == ORDERING::ColMajor) {
-                    auto data_tmp = data_ + i * cols_;
+        auto Row(size_t i)
+        {
+            if constexpr (ORD == ORDERING::ColMajor)
+            {
+                auto data_tmp = data_ + i * cols_;
 
-                    return VectorView(cols_, 1, data_tmp);
-                } else {
-                    return Transpose(this).Row(i);
-                }
-        } 
-
-        auto Col(size_t i) {
-                if constexpr (ORD == ORDERING::ColMajor) {
-                    return this.Row(i);
-                } else {
-                    return Transpose(this).Col(i);
-                }
-        }
-        
-        auto Rows(size_t first, size_t next) const {
-            if constexpr (ORD == ORDERING::ColMajor) {
-                return MatrixView(next-first, cols_, data_+first);
-            } else {
-                return MatrixView(next-first, cols_, data_+first*cols_);
+                return VectorView(cols_, 1, data_tmp);
+            }
+            else
+            {
+                return Transpose(this).Row(i);
             }
         }
 
-        auto Cols(size_t first, size_t next) const {
-            if constexpr (ORD == ORDERING::ColMajor) {
-                return MatrixView(rows_, next-first, data_+first*rows_);
-            } else {
-                return MatrixView(rows_, next-first, data_+first);
+        auto Col(size_t i)
+        {
+            if constexpr (ORD == ORDERING::ColMajor)
+            {
+                return this.Row(i);
+            }
+            else
+            {
+                return Transpose(this).Col(i);
+            }
+        }
+
+        auto Rows(size_t first, size_t next) const
+        {
+            if constexpr (ORD == ORDERING::ColMajor)
+            {
+                return MatrixView(next - first, cols_, data_ + first);
+            }
+            else
+            {
+                return MatrixView(next - first, cols_, data_ + first * cols_);
+            }
+        }
+
+        auto Cols(size_t first, size_t next) const
+        {
+            if constexpr (ORD == ORDERING::ColMajor)
+            {
+                return MatrixView(rows_, next - first, data_ + first * rows_);
+            }
+            else
+            {
+                return MatrixView(rows_, next - first, data_ + first);
             }
         }
     };
 
     template <typename T, ORDERING ORD>
-    class Matrix : MatrixView<T, ORD> {
+    class Matrix : MatrixView<T, ORD>
+    {
         size_t rows_;
         size_t cols_;
         Vector<T> data_;
 
     public:
         // Constructors
-        Matrix(size_t rows, size_t cols) : MatrixView<T, ORD>(rows, cols, new Vector<T>(rows * cols)) { ; }
+        Matrix(size_t rows, size_t cols) : MatrixView<T, ORD>(rows, cols, &data_), rows_(rows), cols_(cols), data_(rows * cols) {}
 
         // Copy constructor
-        Matrix(const Matrix& other) : rows_(other.rows_), cols_(other.cols_), data_(other.data_) {}
+        Matrix(const Matrix &other) : rows_(other.rows_), cols_(other.cols_), data_(other.data_) {}
 
         // Move constructor
-        Matrix(Matrix&& other) noexcept : rows_(other.rows_), cols_(other.cols_), data_(std::move(other.data_)) {}
+        Matrix(Matrix &&other) noexcept : rows_(other.rows_), cols_(other.cols_), data_(std::move(other.data_)) {}
 
         // Destructor
         ~Matrix() = default;
 
         // Access operator for element (i, j)
-T& operator()(size_t i, size_t j) {
-    if constexpr (ORD == ORDERING::ColMajor) {
-                return data_(i +j * rows_);
-            } else {
+        T &operator()(size_t i, size_t j)
+        {
+            if constexpr (ORD == ORDERING::ColMajor)
+            {
+                return data_(i + j * rows_);
+            }
+            else
+            {
                 return data_(j + i * cols_);
             }
-}
+        }
 
-// Const version of the access operator
-const T& operator()(size_t i, size_t j) const {
-    if constexpr (ORD == ORDERING::ColMajor) {
+        // Const version of the access operator
+        const T &operator()(size_t i, size_t j) const
+        {
+            if constexpr (ORD == ORDERING::ColMajor)
+            {
                 return data_[i](j * rows_);
-            } else {
+            }
+            else
+            {
                 return data_[j](i * cols_);
             }
-}
-
+        }
 
         // Assignment operator
-        Matrix& operator=(const Matrix& other) {
-            if (this != &other) {
+        Matrix &operator=(const Matrix &other)
+        {
+            if (this != &other)
+            {
                 rows_ = other.rows_;
                 cols_ = other.cols_;
                 data_ = other.data_;
@@ -151,18 +189,26 @@ const T& operator()(size_t i, size_t j) const {
         }
 
         // Output stream operator for easy printing
-        friend std::ostream& operator<<(std::ostream& os, const Matrix& matrix) {
-            for (size_t i = 0; i < matrix.rows_; ++i) {
-                if (i > 0) {
+        friend std::ostream &operator<<(std::ostream &os, const Matrix &matrix)
+        {
+            for (size_t i = 0; i < matrix.rows_; ++i)
+            {
+                if (i > 0)
+                {
                     os << "\n";
                 }
-                for (size_t j = 0; j < matrix.cols_; ++j) {
-                    if (j > 0) {
+                for (size_t j = 0; j < matrix.cols_; ++j)
+                {
+                    if (j > 0)
+                    {
                         os << " ";
                     }
-                    if constexpr (ORD == ORDERING::ColMajor) {
+                    if constexpr (ORD == ORDERING::ColMajor)
+                    {
                         os << matrix.data_(i + j * matrix.rows_);
-                    } else {
+                    }
+                    else
+                    {
                         os << matrix.data_(j + i * matrix.cols_);
                     }
                 }
@@ -170,66 +216,87 @@ const T& operator()(size_t i, size_t j) const {
             return os;
         }
         // Matrix-Matrix Multiplication
-Matrix operator*(const Matrix& other) const {
-    if (cols_ != other.rows_) {
-        // Invalid multiplication, return an empty matrix or throw an exception
-        return Matrix(0, 0);
-    }
+        Matrix operator*(const Matrix &other) const
+        {
+            if (cols_ != other.rows_)
+            {
+                // Invalid multiplication, return an empty matrix or throw an exception
+                return Matrix(0, 0);
+            }
 
-    Matrix result(rows_, other.cols_);
+            Matrix result(rows_, other.cols_);
 
-    for (size_t i = 0; i < rows_; ++i) {
-        for (size_t j = 0; j < other.cols_; ++j) {
-            T sum = 0;
-            for (size_t k = 0; k < cols_; ++k) {
-                if constexpr (ORD == ORDERING::ColMajor) {
-                    sum += (*this)(i, k) * other(k, j);
-                } else {
-                    sum += (*this)(i, k) * other(k, j);
+            for (size_t i = 0; i < rows_; ++i)
+            {
+                for (size_t j = 0; j < other.cols_; ++j)
+                {
+                    T sum = 0;
+                    for (size_t k = 0; k < cols_; ++k)
+                    {
+                        if constexpr (ORD == ORDERING::ColMajor)
+                        {
+                            sum += (*this)(i, k) * other(k, j);
+                        }
+                        else
+                        {
+                            sum += (*this)(i, k) * other(k, j);
+                        }
+                    }
+                    result(i, j) = sum;
                 }
             }
-            result(i, j) = sum;
+            return result;
         }
-    }
-    return result;
-}
-// Transpose method
-        Matrix<T, ORD> transpose() const {
+        // Transpose method
+        Matrix<T, ORD> transpose() const
+        {
             Matrix<T, ORD> result(cols_, rows_);
-            for (size_t i = 0; i < rows_; ++i) {
-                for (size_t j = 0; j < cols_; ++j) {
+            for (size_t i = 0; i < rows_; ++i)
+            {
+                for (size_t j = 0; j < cols_; ++j)
+                {
                     result(j, i) = (*this)(i, j);
                 }
             }
             return result;
         }
         // Matrix-Vector Multiplication
-        Vector<T> operator*(const Vector<T>& vector) const {
-            if constexpr (ORD == ORDERING::ColMajor) {
-                if (cols_ != vector.Size()) {
+        Vector<T> operator*(const Vector<T> &vector) const
+        {
+            if constexpr (ORD == ORDERING::ColMajor)
+            {
+                if (cols_ != vector.Size())
+                {
                     // Invalid multiplication, return an empty vector or throw an exception
                     return Vector<T>(0);
                 }
 
                 Vector<T> result(rows_);
-                for (size_t i = 0; i < rows_; ++i) {
+                for (size_t i = 0; i < rows_; ++i)
+                {
                     T sum = 0;
-                    for (size_t j = 0; j < cols_; ++j) {
+                    for (size_t j = 0; j < cols_; ++j)
+                    {
                         sum += (*this)(i, j) * vector(j);
                     }
                     result(i) = sum;
                 }
                 return result;
-            } else {
-                if (rows_ != vector.Size()) {
+            }
+            else
+            {
+                if (rows_ != vector.Size())
+                {
                     // Invalid multiplication, return an empty vector or throw an exception
                     return Vector<T>(0);
                 }
 
                 Vector<T> result(cols_);
-                for (size_t j = 0; j < cols_; ++j) {
+                for (size_t j = 0; j < cols_; ++j)
+                {
                     T sum = 0;
-                    for (size_t i = 0; i < rows_; ++i) {
+                    for (size_t i = 0; i < rows_; ++i)
+                    {
                         sum += (*this)(i, j) * vector(i);
                     }
                     result(j) = sum;
--- tests/test_vector.cc
+++ tests/test_vector.cc
@@ -5,38 +5,35 @@
 
 namespace bla = ASC_bla;
 
-
 int main()
 {
   size_t n = 10;
   bla::Vector<double> x(n), y(n);
 
-  bla::Matrix<double, bla::ORDERING::ColMajor> A(n,n);
+  bla::Matrix<double, bla::ORDERING::ColMajor> A(n, n);
 
   for (size_t i = 0; i < x.Size(); i++)
-    {
-      x(i) = i;
-      y(i) = 10;
-    }
+  {
+    x(i) = i;
+    y(i) = 10;
+  }
 
   for (size_t i = 0; i < A.Size_Rows(); i++)
     for (size_t j = 0; j < A.Size_Cols(); j++)
-      A(i,j) = i+j;
+      A(i, j) = i + j;
 
-  bla::Vector<double> z = x+y;
-  
+  bla::Vector<double> z = x + y;
+
   std::cout << "x+y = " << z << std::endl;
-  
-  std::cout << "type of (x+3*y) is  " << typeid(x+3*y).name() << std::endl;
 
-  std::cout << "x+3*y = " << x+3*y << std::endl;
+  std::cout << "type of (x+3*y) is  " << typeid(x + 3 * y).name() << std::endl;
 
-  std::cout << "sizeof(x+3*y) = " << sizeof(x+3*y) << std::endl;
-  
-  x.Range(2,9) = 3;
-  x.Slice(1,5) = 10;
-  
-  std::cout << "x = " << x << std::endl;  
+  std::cout << "x+3*y = " << x + 3 * y << std::endl;
+
+  std::cout << "sizeof(x+3*y) = " << sizeof(x + 3 * y) << std::endl;
+
+  x.Range(2, 9) = 3;
+  x.Slice(1, 5) = 10;
+
+  std::cout << "x = " << x << std::endl;
 }
-
-
